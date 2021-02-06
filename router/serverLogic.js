100f301d543cbfc8d436d1d312ab251023dc6020
Jade <holidayexplanation@gmail.com>
2021-02-06 14:55:44 +0100

    *

--- router/serverLogic.js
+++ router/serverLogic.js
@@ -1,11 +1,35 @@
 const express = require('express')
 const auth = require('../middleware/auth')
+const firebase = require('firebase');
 const steamprice = require('steam-price-api');
 const router = new express.Router()
 const moment = require('moment')
 const User = require('../models/user')
 const Skin = require('../models/skin')
 const log = console.log
+
+firebase.initializeApp({ 
+  apiKey: "AIzaSyDMWb56THHAiz7jqRT2dyDbIq2R3ux3Mp0",
+  authDomain: "revo-skins.firebaseapp.com",
+  projectId: "revo-skins",
+  storageBucket: "revo-skins.appspot.com",
+  messagingSenderId: "850588837597",
+  appId: "1:850588837597:web:ef961e5390f73558241d34",
+  measurementId: "G-G43NV0CTSV"
+})
+
+require('firebase/firestore')
+
+const firestore = firebase.firestore()
+
+firestore.collection("drops").add({
+  uname: 'John',
+  skin: 'Redline',
+  skin_longhand: 'ak-47_redline',
+  grade: 'classified',
+  condition: 'fn',
+  timeOpened: Number(Date.now())
+})
 
 router.get('/', async(req, res) => {
   return res.status(200).send('Server active.')
