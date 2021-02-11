0df7fcde8fa842d66f49da66597380a142721597
Jade <holidayexplanation@gmail.com>
2021-02-11 13:41:28 +0100

    Update user.js

--- models/user.js
+++ models/user.js
@@ -63,14 +63,12 @@ const userSchema = new mongoose.Schema({
       trim: true,
       required: false
     },
-    googleAdsFinishedAt: {
-      type: Date,
-      trim: true,
-      required: false
+    googleAdsFinished: {
+      type: Boolean,
+      default: false
     },
     adsFinished: {
       type: Boolean,
-      trim: true,
       required: false
     },
     tokens: [{
