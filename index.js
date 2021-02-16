15d9ee8cbddac3f70c61fba989d49165547777c5
Jade <holidayexplanation@gmail.com>
2021-02-16 19:21:25 +0100

    *

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
-const app = express();
+const app = express()
+const compression = require('compression')
 
 const serverLogic = require('./router/serverLogic')
 
@@ -25,6 +26,8 @@ app.use(cors({
 }))
 
 app.use(express.json())
+
+app.use(compression())
 
 app.use(serverLogic)
 
